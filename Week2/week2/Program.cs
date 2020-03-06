cf4002b6e95d776204642c380a6ceb6be80bc6bf
JOHN SAHAGUN <102971917@student.swin.edu.au>
2020-03-06 15:23:34 +1100

    week2

---
+++ Week2/week2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Week2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string StaticUserName = ("YeetusDeletus");
+            const string StaticPassword = ("asdfghjkl");
+
+
+        Console.WriteLine("Enter username:");
+        string UserName = Console.ReadLine();
+        Console.WriteLine("Enter password:");
+        string Password = Console.ReadLine();
+
+
+        if (UserName == StaticUserName)
+        {
+            if (Password == StaticPassword)
+            {
+          Console.WriteLine("Login Successful");
+
+            }
+            else
+            {
+         Console.WriteLine("Login Unsuccessful");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Login Unsuccessful");   
+        }
+        }
+    }
+}
+
+
+        
