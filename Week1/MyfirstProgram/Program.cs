9bfd664f04f285e902658ae8da3696496700620b
JOHN SAHAGUN <102971917@student.swin.edu.au>
2020-03-06 12:45:45 +1100

    week2

---
+++ Week1/MyfirstProgram/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace repos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your given name?");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine("What is your surname name?");
+
+            string surname = Console.ReadLine();
+            
+            Console.WriteLine("What is your studentID?");
+
+            int studentID = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("When is your year of birth?");
+
+            int year = Convert.ToInt32(Console.ReadLine());
+
+             Console.WriteLine("What is the current year?");
+
+             int Cyear = Convert.ToInt32(Console.ReadLine());
+
+            
+            string message = "OH so your full name is " + name + " " + surname + 
+            ", and your studentID is " + studentID + " and your year of birth is " + year + 
+            " and the current year is " + Cyear + " and your current age is ";
+
+            Console.WriteLine(message +((Cyear - year)));
+            
+            Console.ReadKey();
+        }
+    }
+}
